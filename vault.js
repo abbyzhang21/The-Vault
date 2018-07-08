e5a0df255abd9f15d2b518808332e182774aff73
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-07-07 16:47:29 -1000

    the-vaulted complete

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+    return myFunc();
 };
+function myFunc(){
+const vaulted ={};
+const getValue = function(key){
+    if(vaulted[key] !== undefined){
+        return vaulted[key];
+    }else{
+        return null;
+    }
+}
+const setValue = function(key, value){
+      if(!(vaulted.key)){
+          return vaulted[key] = value;
+      }
+    
+}
+
+return { 
+    getValue: getValue,
+    setValue: setValue,
+    vaulted: vaulted
+}
+}
